baa8ed1c499bd626795111dce48da602482ab0a7
Gabriel Katz <gabreiel.katz223@gmail.com>
2017-09-22 15:00:02 -0700

    Edited res

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const helloController = require('./helloController');
 
 app.get('/hello', helloController);
 
-app.get('/', (req, res) => res.json({ hello: 'world' }));
+app.get('/', (req, res) => res.json({ hello: 'How are you?' }));
+
+console.log("have you been alright?")
 
 app.listen(3000);
